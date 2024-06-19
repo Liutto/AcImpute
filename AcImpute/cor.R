e5f147bcd48254259ef2d7e975eeb9a237c63f20
Liutto <132654474+Liutto@users.noreply.github.com>
2024-06-19 23:19:18 +0800

    Add files via upload

--- AcImpute/cor.R
+++ AcImpute/cor.R
@@ -6,7 +6,7 @@ raw = read.csv(count_path)
 for (mtd in allmtd){
   print(mtd)
   sexpr = readRDS(paste0('E:/学习箱/论文/2/datasets/cellbench/sc_10x_5cl/methods/',mtd,'/sc_10x_5cl.rds'))
-  if (mtd=='AcImpute'||mtd=='Acmagic'||mtd=='magic'||mtd=="deepimpute"||mtd=='Acmagic_np'){
+  if (mtd=='AcImpute'){
     rownames(sexpr) <- sexpr[, 1]
     sexpr<- sexpr[, -1]
   }
@@ -28,11 +28,9 @@ for (mtd in allmtd){
 allf <- list.files('E:/学习箱/论文/2/datasets/cellbench/sc_10x_5cl/cor')
 res <- lapply(allf,function(f) {
   tmp <- readRDS(paste0('E:/学习箱/论文/2/datasets/cellbench/sc_10x_5cl/cor/',f))
-  # sapply(tmp,median)
-})
+ })
 
 names(res) <- sub('.rds','',allf)
-# res <- res[sapply(res,length) > 20]
 cn <- table(unlist(sapply(res,names)))
 cn <- names(cn)[cn==length(res)]
 
@@ -45,7 +43,6 @@ mp <- apply(res,2,median,na.rm=T)
 pd <- melt(res)
 colnames(pd) <- c('cell','Method',"Correlation")
 pd[,'Method'] <- factor(as.character(pd[,'Method']),levels = c('raw',setdiff(names(sort(mp,decreasing = T)),'raw')))
-# pdf('/home-4/whou10@jhu.edu/scratch/Wenpin/rna_imputation/plot/plot/correlation_mca.pdf',width=5,height=5)
 # install.packages("ggpubr")
 library(ggpubr)
 ########
@@ -54,7 +51,6 @@ type5<-c("A549","HCC827","H1975","H2228","H838")
 pd[, 1]=gsub("\\..*", "", pd[, 1])
 
 pd=pd[pd$Method%in%c("AcImpute","raw"),]
-# 将Condition列转换为因子，并重新设置水平
 pd$Method <- factor(pd$Method, levels =c("AcImpute","raw"))
 par(font.axis = 200)
 # 绘制小提琴图和箱线图
@@ -74,7 +70,7 @@ p <- ggplot(pd, aes(x = cell, y = Correlation, fill = Method)) +
   theme(legend.position = "top")+
   stat_compare_means(aes(group = Method),method = 't.test',label = 'p.signif')
 p
-ggsave(p,filename = "E:/学习箱/论文/2/cor.pdf",width = 12,height = 9)
+ggsave(p,filename = "cor.pdf",width = 12,height = 9)
 
 ########################################################
 #箱线图
@@ -109,34 +105,32 @@ library("xlsx")
 dataercclong<-c()
 datasets<-c("SmartSeq2","CELseq2","MARSseq","SCRBseq","SmartSeq")
 for (i in 1:length(datasets)) {
-  eval(parse(text = paste0("Acmagic<-read.csv('E:/学习箱/论文/2/datasets/Ziegenhain/",datasets[i],"_Acmagic_np.csv',header = T)")))
-  
-  # eval(parse(text = paste0("Acmagic<-read.csv('E:/学习箱/论文/2/datasets/Ziegenhain/",datasets[i],"_procv_nor_Acmagicrenormalt0.csv',header = T)")))
-  eval(parse(text = paste0(datasets[i],"rawdata<-read.csv('E:/学习箱/论文/2/datasets/Ziegenhain/",datasets[i],".csv',header = T)")))
+  eval(parse(text = paste0("AcImpute<-read.csv('E:/学习箱/论文/2/code/data/",datasets[i],"_AcImpute.csv',header = T)")))
+   eval(parse(text = paste0(datasets[i],"rawdata<-read.csv('E:/学习箱/论文/2/datasets/Ziegenhain/",datasets[i],".csv',header = T)")))
   eval(parse(text = paste0("erccrawloc = grep('ERCC',",datasets[i],"rawdata[,1])")))
   eval(parse(text = paste0("erccraw = ",datasets[i],"rawdata[erccrawloc,]")))
   ercc<-read.xlsx("E:/学习箱/论文/2/datasets/Ziegenhain/ERCC.xlsx",1)
   ercc = as.matrix(ercc[,c(2,4)])
   
-  erccAcmagic = Acmagic[grep('ERCC',Acmagic[,1]),]
-  common_rows <- intersect(erccraw[, 1], erccAcmagic[, 1])
+  erccAcImpute = AcImpute[grep('ERCC',AcImpute[,1]),]
+  common_rows <- intersect(erccraw[, 1], erccAcImpute[, 1])
   
-  erccAcmagic = erccAcmagic[erccAcmagic[,1]%in%common_rows,]
+  erccAcImpute = erccAcImpute[erccAcImpute[,1]%in%common_rows,]
   erccraw = erccraw[erccraw[,1]%in%common_rows,]
   ercc = ercc[ercc[,1]%in%gsub("g", "", common_rows),]
-  erccAcmagic = erccAcmagic[order(erccAcmagic[,1]),]
+  erccAcImpute = erccAcImpute[order(erccAcImpute[,1]),]
   erccraw = erccraw[order(erccraw[,1]),]
   ercc = ercc[order(ercc[,1]),]
   
-  erccAcmagic  = apply(erccAcmagic[,-1],2,as.numeric)
+  erccAcImpute  = apply(erccAcImpute[,-1],2,as.numeric)
   ercc = as.numeric(ercc[,2])
   
   erccrawcor = corercc(log(ercc),log(erccraw[,-1]+1))
-  erccAcmagic = corercc(log(ercc),log(erccAcmagic+1))
+  erccAcImpute = corercc(log(ercc),log(erccAcImpute+1))
   
-  dataercc=cbind(erccrawcor,erccAcmagic)
+  dataercc=cbind(erccrawcor,erccAcImpute)
   dataercc=data.frame(dataercc)
-  colnames(dataercc)<-c("raw","Acmagic")
+  colnames(dataercc)<-c("raw","AcImpute")
   dataercc <- dataercc %>% 
     gather(key = 'Ziegenhain',value = 'correlation') #命名两个列名
   eval(parse(text = paste0("methods<-rep('",datasets[i],"',length(erccrawcor))")))
@@ -144,7 +138,7 @@ for (i in 1:length(datasets)) {
   dataercclong = rbind(dataercclong,dataercctmp)
   #内容没有 格式转换
   # InsectSprays %>% {
-  eval(parse(text = paste0("p",datasets[i],"<- ggplot(dataercc,mapping = aes(x = '',y = correlation , fill = factor(Ziegenhain,levels = c('raw','Acmagic')) ))+
+  eval(parse(text = paste0("p",datasets[i],"<- ggplot(dataercc,mapping = aes(x = '',y = correlation , fill = factor(Ziegenhain,levels = c('raw','AcImpute')) ))+
      geom_boxplot(outlier.shape = 21,outlier.colour = 'red',outlier.fill ='blue',
                                       col = 'black',width=1.2)+#,fill = brewer.pal(7,'Paired')
      theme_bw()+
